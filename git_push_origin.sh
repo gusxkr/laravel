e93d3e98b1dbb0a8093afd0482847b12167e3b19
docker_local <docker_local>
2022-04-26 15:26:40 +0900

    aaa

--- git_push_origin.sh
+++ git_push_origin.sh
@@ -2,7 +2,8 @@
 
 github_id="hj"
 github_token="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"
-github_address="https://github.com/siliconii-hanjin/test.git"
+github_address="github.com/hanjinjung/laravel.git"
 
-git push https://$github_id:$github_Token@$github_Address
+git push https://$github_id:$github_token@$github_address
+
 
