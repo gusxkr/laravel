ef7a3dc65fe9db10f7d2adffb62cd581306eb05d
docker_local <docker_local>
2022-04-26 15:57:51 +0900

    for security

--- git_push_origin.sh
+++ git_push_origin.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #sample code
-github_id="test"
-github_token="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"
+github_id=""
+github_token=""
 github_address="github.com/hanjinjung/laravel.git"
 
 git push https://$github_id:$github_token@$github_address
